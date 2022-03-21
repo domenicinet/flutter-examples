df707e968650fa0498e308a3f8f2035ebdf5586a
Alex Domenici <alx.domenici@gmail.com>
2022-03-21 23:02:25 +0700

    Edited example 1

--- src/example-1/main.dart
+++ src/example-1/main.dart
@@ -5,16 +5,12 @@ class ExampleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(' Creating a Dartpad with a repository!'),
+        title: Text('Flutter Example 1'),
       ),
       body: Center(
         child: Column(
           children: [
-            Text('gh_owner: Owner of the Github Account. In thise case is is: Jose Alba '),
-            Text('gh_repo: Name of the repo within the above account. In this case it is: flutter_code'),
-            Text('gh_path: Path to a dartpad_metadata.yaml file within the repo. In this case it is: lib/dartpad'),
-            Text('Collectivaly to make a dartpad with this repository the URL would be: '),
-            Text('dartpad.dev/embed-flutter.html?gh_owner=JoseAlba&gh_repo=flutter_code&gh_path=lib/dartpad'),
+            Text('This is Flutter example #1'),
           ],
         ),
       ),
